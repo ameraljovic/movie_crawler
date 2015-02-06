3dedc9c7bb25a3764152cc2c33221b85cf66c187
ameraljovic <ammer.aljovic@gmail.com>
2015-02-06 19:35:08 +0100

    Refactoring

--- src/main/java/ba/aljovic/amer/configuration/DatabaseProperties.java
+++ src/main/java/ba/aljovic/amer/configuration/DatabaseProperties.java
@@ -1,4 +1,4 @@
-package ba.aljovic.amer.application.database;
+package ba.aljovic.amer.configuration;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
